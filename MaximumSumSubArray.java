2180d8d83a5e350d4a801b59243b7c93d4683fa0
Binod Sarki <147014832+BonodSarki69@users.noreply.github.com>
2025-01-13 21:10:21 +0545

    Add files via upload

---
+++ MaximumSumSubArray.java
@@ -0,0 +1,25 @@
+package Array;
+
+//this solution is about how to find the maximum sum of the subarray formed under array a by the size of w = 4. time complexity is o(n^2).
+//like subarray can be formed as [3, 8, 2, 5], [8, 2, 5, 7], etc
+public class MaximumSumSubArray {
+    public static void main(String[] args) {
+        // int a [] = {3, 8, 2, 5, 7, 6, 12};
+        // int w = 4;
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0; i<=a.length - w; i++){
+        //     int current = 0;
+        //     for(int j=i; j<=i+w-1; j++){
+        //         current = current + a[j];
+        //     }
+        //     if(max<current){
+        //         max = current;
+        //     }
+        // }
+        // System.out.println(max);
+
+
+        //sliding window technique
+
+    }
+}
